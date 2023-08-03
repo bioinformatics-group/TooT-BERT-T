9e240be51aae921e97f616d0d5f4f87568a2297d
Stuart Thiel <stuart.thiel@gmail.com>
2023-08-03 10:34:46 -0400

    Last round of updates
     * Added citation for the tool
     * Normalized string formatting
     * Track errors per sequence, continuing after logging
     * Fixed args to support problem sequence output file and gave up on
    trying to give a dynamic list of available tokenizers and just suggested
    a place to look... most folks will use defaults anyway.

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ warnings.filterwarnings("ignore")
 import logging
 logging.getLogger("transformers.modeling_utils").setLevel(logging.ERROR)
 
+import re
 import argparse
 import joblib
 from Bio import SeqIO
@@ -14,17 +15,38 @@ from transformers import BertTokenizer, BertModel
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 
-# Get list of available pre-trained tokenizers
-tokenizer_list = BertTokenizer.list_pretrained_tokenizers()
+parser = argparse.ArgumentParser(description="""A tool to classify transporter proteins using a BERT-based model.
+Citation:
+@InProceedings{10.1007/978-3-031-17024-9_1,
+author="Ghazikhani, Hamed
+and Butler, Gregory",
+editor="Fdez-Riverola, Florentino
+and Rocha, Miguel
+and Mohamad, Mohd Saberi
+and Caraiman, Simona
+and Gil-Gonz{\'a}lez, Ana Bel{\'e}n",
+title="TooT-BERT-T: A BERT Approach on Discriminating Transport Proteins from Non-transport Proteins",
+booktitle="Practical Applications of Computational Biology and Bioinformatics, 16th International Conference (PACBB 2022)",
+year="2023",
+publisher="Springer International Publishing",
+address="Cham",
+pages="1--11",
+abstract="Transmembrane transport proteins (transporters) serve a crucial role for the transport of hydrophilic molecules across hydrophobic membranes in every living cell. The structures and functions of many membrane proteins are unknown due to the enormous effort required to characterize them. This article proposes TooT-BERT-T, a technique that employs the BERT representation to analyze and discriminate between transporters and non-transporters using a Logistic Regression classifier. Additionally, we evaluate frozen and fine-tuned representations from two different BERT models. Compared to state-of-the-art prediction methods, TooT-BERT-T achieves the highest accuracy of 93.89{\%} and MCC of 0.86.",
+isbn="978-3-031-17024-9"
+}""")
 
-parser = argparse.ArgumentParser(description="A tool to classify transporter proteins using a BERT-based model.")
 parser.add_argument('input_file', type=str, help='Input FASTA file')
 parser.add_argument('output_file', type=str, help='Output txt file with predicted labels')
 parser.add_argument("-max_seq_len", help="maximum sequence length", type=int, default=20000)
 parser.add_argument("-lr_model", help="path to the logistic regression model file", default="lr_model.pkl")
-parser.add_argument('-tokenizer', type=str, choices=tokenizer_list, default='rostlab/prot_bert_bfd', help='Choose a tokenizer from the following options: {}'.format(tokenizer_list))
+parser.add_argument('-tokenizer', type=str, default='rostlab/prot_bert_bfd', help='Specify the tokenizer to use. Refer to the Hugging Face model hub for available options.')
+parser.add_argument("-problem_file", help="File to log problematic sequences", default="problem-sequences")
 
 args = parser.parse_args()
+
+# Set the default problem_file if not provided
+if args.problem_file == "problem-sequences":
+    args.problem_file = f"{args.output_file}.{args.problem_file}"
 
 # We check if the input file is a fasta file. fasta file starts with > and then the id and then the sequence.
 with open(args.input_file, 'r') as f:
@@ -52,36 +74,40 @@ lr = joblib.load(args.lr_model)
 # For each sequence, we tokenize it and then we pass it through the BERT model.
 print("Sequence ID\t\tPredicted label")
 print("------------\t\t---------------")
-with open(args.output_file, 'w') as f:
+with open(args.output_file, 'w') as f, open(args.problem_file, 'w') as problem_file:
     for sequence, id in sequences_ids:
-        # Make space between each amino acid.
-        sequence = ' '.join(sequence)
-        # Replace unknown amino acids with X.
-        sequence = sequence.replace('U', 'X')
-        sequence = sequence.replace('O', 'X')
-        sequence = sequence.replace('B', 'X')
-        sequence = sequence.replace('Z', 'X')
+        try:
 
-        tokenized_sequence = tokenizer.encode_plus(sequence, add_special_tokens=True, max_length=args.max_seq_len, truncation=True)
-        input_ids = torch.tensor([tokenized_sequence['input_ids']]).to(device)
-        attention_mask = torch.tensor([tokenized_sequence['attention_mask']]).to(device)
+            # Make space between each amino acid.
+            sequence = ' '.join(sequence)
+            # Replace uncommon amino acids with X.
+            sequence = re.sub('[UOBZ]', 'X', sequence)
 
-        with torch.no_grad():
-            last_hidden_states = model(input_ids, attention_mask=attention_mask)[0]
+            tokenized_sequence = tokenizer.encode_plus(sequence, add_special_tokens=True, max_length=args.max_seq_len, truncation=True)
+            input_ids = torch.tensor([tokenized_sequence['input_ids']]).to(device)
+            attention_mask = torch.tensor([tokenized_sequence['attention_mask']]).to(device)
 
-        embedding = last_hidden_states[0].cpu().numpy()
+            with torch.no_grad():
+                last_hidden_states = model(input_ids, attention_mask=attention_mask)[0]
 
-        seq_len = (attention_mask[0] == 1).sum()
-        seq_embedding = embedding[1:seq_len-1]
-        mean_pool = np.mean(seq_embedding, axis=0)
+            embedding = last_hidden_states[0].cpu().numpy()
 
-        # We predict the label.
-        prediction = lr.predict([mean_pool])
-        # We write the output to the output file.
-        f.write("Sequence:{}\tPrediction:{}".format(id, prediction[0]) + '\n')
-        f.flush()
+            seq_len = (attention_mask[0] == 1).sum()
+            seq_embedding = embedding[1:seq_len-1]
+            mean_pool = np.mean(seq_embedding, axis=0)
 
-        # We print the id and the prediction.
-        print("{}\t{}".format(id, prediction[0]))
+            # We predict the label.
+            prediction = lr.predict([mean_pool])
+            # We write the output to the output file.
+            f.write(f"Sequence:{id}\tPrediction:{prediction[0]}\n")
+            f.flush()
+
+            # We print the id and the prediction.
+            print(f"{id}\t{prediction[0]}")
+
+        except Exception as e:
+            problem_file.write(f"Problem with sequence {id}: {str(e)}\n")
+            problem_file.flush()
+            print(f"Problem with sequence {id}, skipping to the next one.")
 
 print('Finished.')
